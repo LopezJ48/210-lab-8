9fac4a4ae5cedc5e3c2799d1625a32550f09e438
LopezJ48 <lopezjio48@gmail.com>
2024-11-06 20:57:32 -0800

    Add files via upload

--- lab 29.cpp
+++ lab 29.cpp
@@ -5,18 +5,27 @@
 
 using namespace std;
 
-void simulateInventory(map<string, int>& inventoryData, int days); //runs simulation of stock through days
-void displayInventory(map<string, int>& inventoryData); //display stock of items in store
-
 void simulateInventory(map<string, int>& inventoryData){
     for (int day = 0; day < days; ++day){
         cout << "Day" << day + 1 << endl; // will run through days with stock of items such as foods,drinks,goods
         for (auto& [category, stock] : inventoryData){
-            int change = ()
+            int change = (rand() % 11) - 10;
+            stock += change;
+            if (stock < 0) stock = 0;
+            cout << category << "Instore: " << stock << endl;
         }
+        cout << endl;
+    }
+}
+
+void displayInventory(map<string, int>& inventoryData){
+    cout << "Inventory check " << endl;
+    for(const auto& [category, stock] : inventoryData){
+        cout << category << "Instore: " << stock << endl;
     }
 }
 
 int main(){
-    
+int days = 5
+
 }
